bdf7a9c76277dee1f45461db5d1913bbad62f42b
Debargha-arch <mukherjeedebargha281@gmail.com>
2023-06-03 00:23:17 +0530

    DAY 1: Completed upto lecture 7

---
+++ telusko/calc/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request) :
+    return render(request, 'home.html', {'name': 'Debargha'})
+
+def add(request) :
+    num1 = int(request.GET["num1"])
+    num2 = int(request.GET["num2"])
+    ans = num1 + num2
+    return render(request, 'result.html', {'ans': ans})
